5df2a428a36c7ce0e40128df0db782c8737a60e8
Jakub Jirutka <jakub@jirutka.cz>
2023-08-27 21:19:56 +0200

    alpkit: Simplify custom schema building using serde_json

--- alpkit/src/internal/schema.rs
+++ alpkit/src/internal/schema.rs
@@ -1,14 +1,11 @@
 use schemars::gen::SchemaGenerator;
-use schemars::schema::{
-    ArrayValidation, InstanceType, ObjectValidation, Schema, SchemaObject, StringValidation,
-    SubschemaValidation,
-};
-use schemars::{JsonSchema, Map};
+use schemars::schema::Schema;
+use schemars::JsonSchema;
+use serde_json::json;
 
 use crate::apkbuild::Secfix;
 use crate::dependency::Dependency;
 use crate::internal::regex;
-use crate::internal::std_ext::Tap;
 
 /// This wrapper type is only used for `schemars`.
 pub(crate) struct Dependencies(Vec<Dependency>);
@@ -18,34 +15,26 @@ impl JsonSchema for Dependencies {
         "Dependencies".to_owned()
     }
 
-    /// Generates schema:
-    /// ```json
-    /// {
-    ///   "anyOf": [{
-    ///     "type": "object",
-    ///     "additionalProperties": false,
-    ///     "patternProperties": {
-    ///       "^[a-zA-Z0-9_.+\\-:/\\[\\]]+$": {
-    ///         "type": "string",
-    ///         "pattern": "^(?:\\*|[<>=~]{1,2} ?[0-9]+(?:\\.[0-9]+)*[a-z]?(?:_[a-z]+[0-9]*)*(?:-r[0-9]+)?)$"
-    ///       }
-    ///     }
-    ///   }, {
-    ///     "type": "array",
-    ///     "items": {
-    ///       "type": "string",
-    ///       "pattern": "^[a-zA-Z0-9_.+\\-:/\\[\\]]+(?:[<>=~]{1,2}[0-9]+(?:\\.[0-9]+)*[a-z]?(?:_[a-z]+[0-9]*)*(?:-r[0-9]+)?)?$"
-    ///     }
-    ///   }]
-    /// }
-    /// ```
     fn json_schema(_: &mut SchemaGenerator) -> Schema {
-        let array = array_of_items(string_with_pattern(&regex::PROVIDER_WITH_CONSTRAINT));
-        let object = object_with_pattern_properties(
-            &regex::PROVIDER,
-            string_with_pattern(&regex::DEP_CONSTRAINT),
-        );
-        any_of(&[array, object])
+        serde_json::from_value(json!({
+            "anyOf": [{
+                "type": "object",
+                "additionalProperties": false,
+                "patternProperties": {
+                    "^[a-zA-Z0-9_.+\\-:/\\[\\]]+$": {
+                        "type": "string",
+                        "pattern": &regex::DEP_CONSTRAINT.to_string(),
+                    },
+                },
+            }, {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "pattern": &regex::PROVIDER_WITH_CONSTRAINT.to_string(),
+                },
+            }]
+        }))
+        .expect("unable to process JSON schema for Dependencies")
     }
 }
 
@@ -57,77 +46,20 @@ impl JsonSchema for Secfixes {
         "Secfixes".to_owned()
     }
 
-    /// Generates schema:
-    /// ```json
-    /// {
-    ///   "type": "object",
-    ///   "additionalProperties": false,
-    ///   "patternProperties": {
-    ///     "^(?:[0-9]+(?:\\.[0-9]+)*[a-z]?(?:_[a-z]+[0-9]*)*-r[0-9]+|0)$": {
-    ///       "type": "array",
-    ///       "items": {
-    ///         "type": "string"
-    ///       }
-    ///     }
-    ///   }
-    /// }
-    /// ```
-    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> Schema {
-        object_with_pattern_properties(
-            &regex::PKGVER_REL_OR_ZERO,
-            gen.subschema_for::<Vec<String>>(),
-        )
+    fn json_schema(_: &mut schemars::gen::SchemaGenerator) -> Schema {
+        serde_json::from_value(json!({
+            "type": "object",
+            "additionalProperties": false,
+            "patternProperties": {
+                &regex::PKGVER_REL_OR_ZERO.to_string(): {
+                    "type": "array",
+                    "items": {
+                        "type": "string",
+                    },
+                },
+            },
+        })).expect("unable to process JSON schema for Secfixes")
     }
-}
-
-fn any_of(subschemas: &[Schema]) -> Schema {
-    SchemaObject {
-        subschemas: Some(Box::new(SubschemaValidation {
-            any_of: Some(subschemas.into()),
-            ..Default::default()
-        })),
-        ..Default::default()
-    }
-    .into()
-}
-
-fn array_of_items(items: Schema) -> Schema {
-    SchemaObject {
-        instance_type: Some(InstanceType::Array.into()),
-        array: Some(Box::new(ArrayValidation {
-            items: Some(items.into()),
-            ..Default::default()
-        })),
-        ..Default::default()
-    }
-    .into()
-}
-
-fn object_with_pattern_properties<P: ToString>(pattern: P, value_schema: Schema) -> Schema {
-    SchemaObject {
-        instance_type: Some(InstanceType::Object.into()),
-        object: Some(Box::new(ObjectValidation {
-            additional_properties: Some(Box::new(Schema::Bool(false))),
-            pattern_properties: Map::new().tap_mut(|map| {
-                map.insert(pattern.to_string(), value_schema);
-            }),
-            ..Default::default()
-        })),
-        ..Default::default()
-    }
-    .into()
-}
-
-fn string_with_pattern<P: ToString>(pattern: P) -> Schema {
-    SchemaObject {
-        instance_type: Some(InstanceType::String.into()),
-        string: Some(Box::new(StringValidation {
-            pattern: Some(pattern.to_string()),
-            ..Default::default()
-        })),
-        ..Default::default()
-    }
-    .into()
 }
 
 #[cfg(test)]
--- alpkit/src/internal/schema.test.rs
+++ alpkit/src/internal/schema.test.rs
@@ -1,76 +1,13 @@
-use indoc::indoc;
-
 use super::*;
-use crate::internal::test_utils::assert;
 
-macro_rules! assert_schema {
-    ($schema:expr, $expected:expr) => {{
-        assert!(serde_json::to_string_pretty(&$schema).unwrap() == $expected);
-    }};
+#[test]
+fn dependencies_schema() {
+    // shouldn't panic
+    Dependencies::json_schema(&mut Default::default());
 }
 
 #[test]
-fn any_of_json() {
-    assert_schema!(
-        any_of(&[string()]),
-        indoc! {r#"
-            {
-              "anyOf": [
-                {
-                  "type": "string"
-                }
-              ]
-            }"#}
-    )
-}
-
-#[test]
-fn array_of_items_json() {
-    assert_schema!(
-        array_of_items(string()),
-        indoc! {r#"
-            {
-              "type": "array",
-              "items": {
-                "type": "string"
-              }
-            }"#}
-    )
-}
-
-#[test]
-fn object_with_pattern_properties_json() {
-    assert_schema!(
-        object_with_pattern_properties("[a-z]", string()),
-        indoc! {r#"
-            {
-              "type": "object",
-              "patternProperties": {
-                "[a-z]": {
-                  "type": "string"
-                }
-              },
-              "additionalProperties": false
-            }"#}
-    )
-}
-
-#[test]
-fn string_with_pattern_json() {
-    assert_schema!(
-        string_with_pattern("[a-z]"),
-        indoc! {r#"
-            {
-              "type": "string",
-              "pattern": "[a-z]"
-            }"#}
-    )
-}
-
-fn string() -> Schema {
-    SchemaObject {
-        instance_type: Some(InstanceType::String.into()),
-        ..Default::default()
-    }
-    .into()
+fn secfixes_schema() {
+    // shouldn't panic
+    Secfixes::json_schema(&mut Default::default());
 }
